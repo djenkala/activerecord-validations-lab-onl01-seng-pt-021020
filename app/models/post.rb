5dd30dcf41244133fbb673811b24605d8aeeb552
Daniel Jenkala <djenkala@yahoo.com>
2020-06-21 23:47:46 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,13 +5,13 @@ class Post < ActiveRecord::Base
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
   validates :is_clickbait?
   
-  CLICKBAIT_PATTERNS = [
-     /Won't Believe/i,
+   CLICKBAIT_PATTERNS = [
+    /Won't Believe/i,
     /Secret/i,
     /Top [0-9]*/i,
     /Guess/i
-    ]
-    
+  ]
+
   def is_clickbait?
     if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
       errors.add(:title, "must be clickbait")
