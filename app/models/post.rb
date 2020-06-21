c47569b0b34c4e6a1bc9081264dd4ead0be53a50
Daniel Jenkala <djenkala@yahoo.com>
2020-06-21 23:44:33 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,6 @@ class Post < ActiveRecord::Base
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
   validates :is_clickbait?
+  
+  CLICKBAIT 
 end
